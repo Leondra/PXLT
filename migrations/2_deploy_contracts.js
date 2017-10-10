a782f72455aa757ca7f7fdfc5dec52e46c4eb7e1
Nikita Fuchs <q.fuchs@gmail.com>
2017-10-11 01:25:10 +0200

    replace deployment variables
    remove the preset variables with blank spaces to be set during migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var PXLToken = artifacts.require("./PXLToken.sol");
 
 module.exports = function(deployer, network, accounts) {
-  var start = 10;
-  var middle = 15;
-  var end = 20;
- deployer.deploy(PXLToken, start, end, middle, accounts[0], accounts[1]);
+  var start = ;
+  var middle = ;
+  var end = ;
+  var admin1 = "";
+  var admin2 = "";
+ deployer.deploy(PXLToken, start, end, middle, admin1, admin2);
 };
